2551c113ef7706ff11f2ef4f0f9581ccfdf7f6ab
Fran López <fclopezjurado@gmail.com>
2016-11-09 20:46:08 +0100

    Rename "TemporalRequestToken" POJO class to "SessionResponse" class.  This class has been renamed to be used to store information of more server response types. A new "sessionID" attribute has been added to manage the session associated to API user.

--- app/src/main/java/es/upm/miw/ratingmanager/api/TMDbAPI.java
+++
@@ -1,8 +0,0 @@
-package es.upm.miw.ratingmanager.api;
-
-/**
- * Created by franlopez on 07/11/2016.
- */
-
-public interface TMDbAPI {
-}
--- app/src/main/java/es/upm/miw/ratingmanager/api/pojos/SessionResponse.java
+++ app/src/main/java/es/upm/miw/ratingmanager/api/pojos/SessionResponse.java
@@ -1,15 +1,16 @@
-package es.upm.miw.ratingmanager.api;
+package es.upm.miw.ratingmanager.api.pojos;
 
 /**
  * Created by franlopez on 07/11/2016.
  */
 
 import javax.annotation.Generated;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 @Generated("org.jsonschema2pojo")
-public class TemporalRequestToken {
+public class SessionResponse {
 
     @SerializedName("success")
     @Expose
@@ -20,59 +21,74 @@ public class TemporalRequestToken {
     @SerializedName("request_token")
     @Expose
     private String requestToken;
+    @SerializedName("session_id")
+    @Expose
+    private String sessionID;
+    @SerializedName("status_message")
 
     /**
-     *
-     * @return
-     * The success
+     * @return The success
      */
     public Boolean getSuccess() {
         return success;
     }
 
     /**
-     *
-     * @param success
-     * The success
+     * @param success The success
      */
     public void setSuccess(Boolean success) {
         this.success = success;
     }
 
     /**
-     *
-     * @return
-     * The expiresAt
+     * @return The expiresAt
      */
     public String getExpiresAt() {
         return expiresAt;
     }
 
     /**
-     *
-     * @param expiresAt
-     * The expires_at
+     * @param expiresAt The expires_at
      */
     public void setExpiresAt(String expiresAt) {
         this.expiresAt = expiresAt;
     }
 
     /**
-     *
-     * @return
-     * The requestToken
+     * @return The requestToken
      */
     public String getRequestToken() {
         return requestToken;
     }
 
     /**
-     *
-     * @param requestToken
-     * The request_token
+     * @param requestToken The request_token
      */
     public void setRequestToken(String requestToken) {
         this.requestToken = requestToken;
     }
 
+    /**
+     * @return The sessionID
+     */
+    public String getSessionID() {
+        return sessionID;
+    }
+
+    /**
+     * @param sessionID The session_id
+     */
+    public void setSessionID(String sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    @Override
+    public String toString() {
+        return "SessionResponse{" +
+                "success=" + success +
+                ", expiresAt='" + expiresAt + '\'' +
+                ", requestToken='" + requestToken + '\'' +
+                ", sessionID='" + sessionID + '\'' +
+                '}';
+    }
 }
