a7db2686c755fc060ac7270d1838b83b1981954f
Fran López <fclopezjurado@gmail.com>
2016-11-15 03:15:50 +0100

    Fixes in "APIManager" class from "es.upm.miw.ratingmanager.api.manager" package to manage asynchronous calls outside.

--- app/src/main/java/es/upm/miw/ratingmanager/api/manager/APIManager.java
+++ app/src/main/java/es/upm/miw/ratingmanager/api/manager/APIManager.java
@@ -6,7 +6,6 @@ import es.upm.miw.ratingmanager.api.pojos.Movies;
 import es.upm.miw.ratingmanager.api.pojos.RatingResponse;
 import es.upm.miw.ratingmanager.api.pojos.SessionResponse;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -29,42 +28,35 @@ public class APIManager {
         this.APIInterface = retrofit.create(TMDbAPI.class);
     }
 
-    public void getTemporalRequestToken(String APIKey, Callback<SessionResponse> callback) {
-        Call<SessionResponse> call = this.APIInterface.getTemporalRequestToken(APIVersion, APIKey);
-        call.enqueue(callback);
+    public Call<SessionResponse> getTemporalRequestToken(String APIKey) {
+        return this.APIInterface.getTemporalRequestToken(APIVersion, APIKey);
     }
 
-    public void validateRequestToken(String APIKey, String userName, String userPass,
-                                     String requestToken, Callback<SessionResponse> callback) {
-        Call<SessionResponse> call = this.APIInterface.validateRequestToken(APIVersion, APIKey,
+    public Call<SessionResponse> validateRequestToken(String APIKey, String userName,
+                                                      String userPass, String requestToken) {
+        return this.APIInterface.validateRequestToken(APIVersion, APIKey,
                 userName, userPass, requestToken);
-        call.enqueue(callback);
     }
 
-    public void getSessionID(String APIKey, String requestToken,
-                             Callback<SessionResponse> callback) {
-        Call<SessionResponse> call = this.APIInterface.getSessionID(APIVersion, APIKey,
+    public Call<SessionResponse> getSessionID(String APIKey, String requestToken) {
+        return this.APIInterface.getSessionID(APIVersion, APIKey,
                 requestToken);
-        call.enqueue(callback);
     }
 
-    public void getGenres(String APIKey, Callback<Genres> callback) {
-        Call<Genres> call = this.APIInterface.getGenres(APIVersion, APIKey);
-        call.enqueue(callback);
+    public Call<Genres> getGenres(String APIKey) {
+        return this.APIInterface.getGenres(APIVersion, APIKey);
     }
 
-    public void getMovies(String APIKey, int page, String primaryReleaseDateGTE,
-                          String primaryReleaseDateLTE, String genresToInclude,
-                          String genresToExclude, Callback<Movies> callback) {
-        Call<Movies> call = this.APIInterface.getMovies(APIVersion, APIKey, page,
+    public Call<Movies> getMovies(String APIKey, int page, String primaryReleaseDateGTE,
+                                  String primaryReleaseDateLTE, String genresToInclude,
+                                  String genresToExclude) {
+        return this.APIInterface.getMovies(APIVersion, APIKey, page,
                 primaryReleaseDateGTE, primaryReleaseDateLTE, genresToInclude, genresToExclude);
-        call.enqueue(callback);
     }
 
-    public void rateMovie(int movieID, String APIKey, String sessionID, double rating,
-                          Callback<RatingResponse> callback) {
-        Call<RatingResponse> call = this.APIInterface.rateMovie(APIVersion, movieID, APIKey,
+    public Call<RatingResponse> rateMovie(int movieID, String APIKey, String sessionID,
+                                          double rating) {
+        return this.APIInterface.rateMovie(APIVersion, movieID, APIKey,
                 sessionID, rating);
-        call.enqueue(callback);
     }
 }
